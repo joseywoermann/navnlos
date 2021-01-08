81fdf6f115960759023e25555730cd22c96db06c
joseywoermann <jcw05@gmx.de>
2021-01-08 23:16:27 +0100

    enabled notifier

--- main.py
+++ main.py
@@ -113,9 +113,9 @@ for filename in os.listdir("./events"):
 for filename in os.listdir("./core"):
     if filename.endswith(".py"):
         client.load_extension(f"core.{filename[:-3]}")
-"""
+
 for filename in os.listdir("./kgh_notifier"):
     if filename.endswith(".py"):
         client.load_extension(f"kgh_notifier.{filename[:-3]}")
-"""
+
 client.run(TOKEN)
