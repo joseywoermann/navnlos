be1ff564e4ef09418ab8a1ca0bcab9d1ebc83bd3
joseywoermann <jcw05@gmx.de>
2021-01-09 13:27:45 +0100

    ping

--- commands/ping.py
+++ commands/ping.py
@@ -8,8 +8,19 @@ class Ping(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def ping(self, ctx):
-        await ctx.send(f"Pong! {round(self.client.latency * 1000)} Millisekunden")
+    async def ping(self, ctx, r=None):
+
+        if r == "precise":
+            ping_embed = discord.Embed(title=f"Pong! {(self.client.latency * 1000)} Millisekunden", colour=discord.Color.gold())
+            ping_embed.set_author(name=str(ctx.author), icon_url=ctx.author.avatar_url)
+
+        else:
+            ping_embed = discord.Embed(title=f"Pong! {round(self.client.latency * 1000)} Millisekunden", colour=discord.Color.gold())
+            ping_embed.set_author(name=str(ctx.author), icon_url=ctx.author.avatar_url)
+
+
+        await ctx.channel.purge(limit=1)
+        await ctx.send(content=None, embed=ping_embed)
 
 def setup(client):
     client.add_cog(Ping(client))
