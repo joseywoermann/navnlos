c273bb21631601b36cf7162818283d57cbe4a821
joseywoermann <jcw05@gmx.de>
2021-02-11 10:36:19 +0100

    added logging

--- commands/developer.py
+++ commands/developer.py
@@ -9,7 +9,7 @@ class Developer(commands.Cog):
 
     @commands.command()
     async def developer(self, ctx):
-        dev_embed = discord.Embed(description="[Twitter](https://twitter.com/joseywoermann/) - [Website](http://joseywoermann.tk/) - [GitHub](https://github.com/joseywoermann/)\n\n[Discord](https://discord.gg/SchJckc) - [Reddit](https://reddit.com/u/joseywoermann/) - [](https://github.com/joseywoermann/)", colour=discord.Colour.dark_grey())
+        dev_embed = discord.Embed(description="[Twitter](https://twitter.com/joseywoermann/) - [Website](https://joseywoermann.ml/) - [GitHub](https://github.com/joseywoermann/)\n\n[Discord](https://discord.gg/SchJckc) - [Reddit](https://reddit.com/u/joseywoermann/) - [Github](https://github.com/joseywoermann/)", colour=discord.Colour.dark_grey())
 
         dev_embed.set_author(name=ctx.message.author,icon_url=ctx.author.avatar_url)
         await ctx.reply(embed = dev_embed)
