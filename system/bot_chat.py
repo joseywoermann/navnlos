97a3760ecfe754afb04a74d701349b207082708b
joseywoermann <jcw05@gmx.de>
2021-04-06 14:56:46 +0200

    added a method to chat through the bot

---
+++ system/bot_chat.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import logging
+
+class Bot_Chat(commands.Cog):
+
+    def __init__(self, client):
+
+        self.client = client
+
+    @commands.command()
+    @commands.is_owner()
+    async def sendto(self, ctx, server, channel, *, text):
+        target = self.client.get_guild(int(server)).get_channel(int(channel))
+        await target.send(str(text))
+        
+        
+        
+    @commands.command()
+    @commands.is_owner()
+    async def sendhere(self, ctx, text):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(str(text))
+
+def setup(client):
+    client.add_cog(Bot_Chat(client))
