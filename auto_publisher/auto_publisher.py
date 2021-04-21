346fe2eb47ec7e8d068816dbe7cbc3bb5c1f872a
Josey C. Wörmann <jcw05@gmx.de>
2021-04-21 21:27:51 +0200

    Update auto_publisher.py

--- auto_publisher/auto_publisher.py
+++ auto_publisher/auto_publisher.py
@@ -14,7 +14,6 @@ class AutoPublisher(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
 
-        server = message.guild
         target_channels = [654347445517549578, 737715494198706286, 741033879032430834, 720674929540071564, 724946319818489946, 685080746410639411, 700345472300089366,739033895365771334, 738838305722073221, 738844667084406814, 738840347656060968]
 
         if message.channel.id in target_channels:
