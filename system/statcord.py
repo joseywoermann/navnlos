2132403b39283de5781453f2fee41afa1e26c40b
joseywoermann <jcw05@gmx.de>
2021-05-25 17:34:47 +0200

    this works

--- system/statcord.py
+++ system/statcord.py
@@ -1,3 +1,4 @@
+"""
 import discord
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
@@ -27,3 +28,35 @@ class StatcordPost(commands.Cog):
 
 def setup(client):
     client.add_cog(StatcordPost(client))
+"""
+
+import discord
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+from main import test_guilds, make_error_embed
+from discord_slash.utils.manage_commands import create_option
+import statcord
+import json
+from statcord import StatcordClient
+
+
+class MyStatcordCog(commands.Cog):
+    def __init__(self, client):
+
+        settings = {}
+
+        with open('settings.json','r') as file:
+            settings = json.load(file)
+            
+        self.client = client
+        self.statcord_client = StatcordClient(client, str(settings["statcordkey"]), self.custom_graph_1)
+
+    def cog_unload(self):
+        self.statcord_client.close()
+
+    async def custom_graph_1(self):
+        return 1 + 2 + 3
+
+
+def setup(client):
+    client.add_cog(MyStatcordCog(client))
