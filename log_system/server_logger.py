37f77de9bddf82b8b15d5376ff6f0b6062d90f87
joseywoermann <jcw05@gmx.de>
2021-05-25 19:06:28 +0200

    removed features that aren't globally available

--- log_system/server_logger.py
+++ log_system/server_logger.py
@@ -8,13 +8,13 @@ class Join_leave_notification(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        jcw05 = self.client.get_user(586206645592391711)
-        await jcw05.send(f"Joined ***{guild.name}*** ({guild.id}).")
+        dev = self.client.get_user(586206645592391711)
+        await dev.send(f"Joined **{guild.name}** ({guild.id}).")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        jcw05 = self.client.get_user(586206645592391711)
-        await jcw05.send(f"Left ***{guild.name}*** ({guild.id}).")
+        dev = self.client.get_user(586206645592391711)
+        await dev.send(f"Left **{guild.name}** ({guild.id}).")
 
 def setup(client):
     client.add_cog(Join_leave_notification(client))
