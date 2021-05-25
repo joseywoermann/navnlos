ad951f34f12f7b62c74c90619f1fe8c6a53fd848
joseywoermann <jcw05@gmx.de>
2021-05-25 21:59:35 +0200

    small tweaks

--- system/stats.py
+++ system/stats.py
@@ -10,25 +10,31 @@ class Info(commands.Cog):
         self.client = client
 
     @cog_ext.cog_slash(
-        name = "stats",
+        name = "info",
         description = "Show statistics",
-        #guild_ids = test_guilds
+        guild_ids = test_guilds
     )
-    async def _stats(self, ctx: SlashContext):
+    async def _info(self, ctx: SlashContext):
         embed = await Info.make(self, ctx)
         await ctx.send(embed=embed)
 
 
     async def make(self, ctx):
         try:
+
+            channels_amount = 0
+            for server in self.client.guilds:
+                for channel in server.channels:
+                    channels_amount = channels_amount + 1
+
             appinfo = await self.client.application_info()
             embed = discord.Embed(title="Information", color=0x75e8ee)
             embed.add_field(name = "Current version:", value = "`V 3.0.0`")
             embed.add_field(name = "Bot Owner:", value = f"{appinfo.owner.mention}")
-            embed.add_field(name = "Used packages:", value = "`32`")
-            embed.add_field(name = "Lines of code:", value = "`3396`")
+            embed.add_field(name = "Written in:", value = "`discord.py`")
             embed.add_field(name = "Commands:", value = f"`{len(self.client.commands)}`")
             embed.add_field(name = "Servers:", value = f"`{len(self.client.guilds)}`")
+            embed.add_field(name = "Channels:", value = f"`{channels_amount}`")
             embed.add_field(name = "Users:", value = f"`{len(self.client.users)}`")
             embed.add_field(name = "Cached messages:", value = f"`{len(self.client.cached_messages)}`")
             embed.set_thumbnail(url = "https://raw.githubusercontent.com/joseywoermann/navnlos/master/assets/icon.PNG")
