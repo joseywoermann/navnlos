e1a48ffa6ff29bc2527bdc1679537b62a3242720
Josey C. Wörmann <jcw05@gmx.de>
2021-05-26 11:44:19 +0200

    fix whitespace

--- system/error.py
+++ system/error.py
@@ -18,7 +18,7 @@ class Error(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, ex):
-        
+
         if not isinstance(ex, commands.CommandNotFound):
             embed = await make_error_embed(ex)
             await ctx.send(embed = embed)
